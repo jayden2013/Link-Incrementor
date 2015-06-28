0a036fbe2f9121dd03fddce419734215536996ca
jayden2013 <jay-weav@hotmail.com>
2015-06-27 18:08:15 -0600

    Link++
    Increment the numbers at the end of URLs, and print them to the screen.

---
+++ LinkIncrementor.java
@@ -0,0 +1,41 @@
+
+/**
+ * 
+ * @author Jayden Weaver
+ *
+ */
+
+public class LinkIncrementor {
+	public static void main(String[] args){
+		if (args.length == 0){
+			System.err.println(usage());
+			System.exit(1);
+		}
+		String URL = args[0];
+		int increment = Integer.parseInt(args[1]), amount = Integer.parseInt(args[2]);
+		int i = 0, numeral = 0;
+		String newURL = "", modURL = "", officialURL = "";
+		modURL += URL.charAt(URL.length() - 3);
+		modURL += URL.charAt(URL.length() - 2);
+		modURL += URL.charAt(URL.length() - 1);
+		numeral = Integer.parseInt(modURL);
+		while (i < URL.length() - 3){
+			newURL += URL.charAt(i);
+			i++;
+		}
+		i = 0;
+		while (i < amount){
+			numeral += increment;
+			officialURL = newURL;
+			officialURL += numeral;
+			System.out.println(officialURL);
+			i++;
+		}
+		
+	}
+	
+	public static String usage(){
+		return "Usage: java LinkGenerator <baseURL> <increment> <amount>";
+	}
+
+}
